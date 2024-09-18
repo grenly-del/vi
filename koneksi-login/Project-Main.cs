be02d58fb2c8ab2244b745cccd262e448924cc80
Jordannst <jordansutarto8@gmail.com>
2024-09-18 20:51:46 +0800

    membuat koneksi login, namun configurasi database masih lokal env

---
+++ koneksi-login/Project-Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace koneksi_login
+{
+    public partial class Project_Main : Form
+    {
+        public Project_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
